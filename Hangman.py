8202cd7420bb27105f0453ea31d9ff652b3ed6f0
Kshitiz <70872155+theNovahkiin@users.noreply.github.com>
2023-08-16 14:05:44 +0545

    Add files via upload
    changes to when user enters previously guessed incorrect word along with addition of list of words for the game

--- Hangman.py
+++ Hangman.py
@@ -1,10 +1,19 @@
+import random
+
 hangman = ["___", " |", " |", " O", "/|\ ", " |", "/ \ "] #hangman parts
-word = "better" #word to guess
+word_lib = [
+    "python", "blanket", "start", "righteous", "brain", "invoke", "submit", "stir", "ring", "recurrent", "bargain", "morning", "night"
+] #list of words for the game
+word_select = random.randint(0, len(word_lib) - 1) #selection of words from random index of the list
+word = word_lib[word_select] #word to guess
 blanks = "_"
 letter_list = list(word) #list of letters in the word
-guess_list = [] #list for adding correct letter guesses by user
+guess_list = [] #list for adding correct letters by user
+incorrect_list = [] #list for adding incorrect letters by user
 hangman_parts = [] #list for adding hangman parts when guess is incorrect
 wrong_guesses = 0 #number of wrong answers
+
+print("WELCOME TO HANGMAN.")
 print("The word is of {} letters.".format(len(word)))
 
 #generating blanks corresponding the number of letters in the word
@@ -22,6 +31,10 @@ while wrong_guesses <= len(hangman) - 1:
     if user == word:
         print("You win!!")
         break
+    #condition for when player enters previously guessed incorrect letter
+    while user in incorrect_list:
+        print("Already guessed. Guess another letter.")
+        user = input("Guess a letter or the whole word: ")
     #condition for checking if the letter player guesses is in the word
     if user in word:
         #loop for getting index of every letter in the word
@@ -34,10 +47,12 @@ while wrong_guesses <= len(hangman) - 1:
             print("You win.")
             print(" ".join(guess_list))
             break
+        print("Correct.")
         print(" ".join(guess_list))
-
+    #condition for when the guess is incorrect
     else:
         print("Incorrect.")
+        incorrect_list.append(user)
         #loop for adding hangman components to a list as incorrect guesses are entered
         hangman_parts.append(hangman[wrong_guesses])
         for i in range(0, len(hangman_parts)):
